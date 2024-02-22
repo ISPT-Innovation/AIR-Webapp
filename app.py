3d072360b5a3847583b840de3eb9b27c1c52d090
hemanthispt <159981263+hemanthispt@users.noreply.github.com>
2024-02-22 20:40:36 +1100

    Update app.py - logging INDEX name

--- app.py
+++ app.py
@@ -294,6 +294,7 @@ def init_cosmosdb_client():
 
 
 def get_configured_data_source():
+    logging.info(AZURE_SEARCH_INDEX)
     data_source = {}
     query_type = "simple"
     if DATASOURCE_TYPE == "AzureCognitiveSearch":
