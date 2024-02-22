bd906c51847ad8e2688a8c69c08bf2214bcf77a3
usr-yanyan <112924060+usr-yanyan@users.noreply.github.com>
2024-02-22 16:37:17 +0900

    Testing frontend input placeholder

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -768,7 +768,7 @@ const Chat = () => {
                             </Stack>
                             <QuestionInput
                                 clearOnSend
-                                placeholder="Type a new question..."
+                                placeholder="Please type a new question..."
                                 disabled={isLoading}
                                 onSend={(question, id) => {
                                     appStateContext?.state.isCosmosDBAvailable?.cosmosDB ? makeApiRequestWithCosmosDB(question, id) : makeApiRequestWithoutCosmosDB(question, id)
