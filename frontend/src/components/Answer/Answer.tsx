18a44f84aafa39008d7f87ed6da207896c901bf3
hemanthispt <159981263+hemanthispt@users.noreply.github.com>
2024-03-14 16:34:01 +1100

    Update Answer.tsx to include the text area - just UI change

--- frontend/src/components/Answer/Answer.tsx
+++ frontend/src/components/Answer/Answer.tsx
@@ -150,11 +150,10 @@ export const Answer = ({
     const UnhelpfulFeedbackContent = () => {
         return (<>
             <div>Why wasn't this response helpful?</div>
-            <Stack tokens={{childrenGap: 4}}>
+            <Stack tokens={{childrenGap: 4}} style={{ marginBottom: '20px' }}>
                 <Checkbox label="Response is inaccurate" id={Feedback.InaccurateOrIrrelevant} defaultChecked={negativeFeedbackList.includes(Feedback.InaccurateOrIrrelevant)} onChange={updateFeedbackList}></Checkbox>
                 <Checkbox label="Response is partially accurate" id={Feedback.PartiallyAccurate} defaultChecked={negativeFeedbackList.includes(Feedback.OutOfScope)} onChange={updateFeedbackList}></Checkbox>
                 <Checkbox label="References are missing" id={Feedback.MissingCitation} defaultChecked={negativeFeedbackList.includes(Feedback.MissingCitation)} onChange={updateFeedbackList}></Checkbox>
-                <Checkbox label="References are inaccurate" id={Feedback.WrongCitation} defaultChecked={negativeFeedbackList.includes(Feedback.WrongCitation)} onChange={updateFeedbackList}></Checkbox>
                 <Checkbox label="References are inaccurate" id={Feedback.WrongCitation} defaultChecked={negativeFeedbackList.includes(Feedback.WrongCitation)} onChange={updateFeedbackList}></Checkbox>
                 <Checkbox label="Response expected but not generated" id={Feedback.ResponseExpectedButNotGenerated} defaultChecked={negativeFeedbackList.includes(Feedback.OtherUnhelpful)} onChange={updateFeedbackList}></Checkbox>
             </Stack>
