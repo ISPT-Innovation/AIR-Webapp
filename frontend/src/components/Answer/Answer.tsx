6899da763e18c8958a8f80794a133e89599d704e
hemanth.bayyarapu <hemanth.bayyarapu@slalom.com>
2024-03-28 10:42:28 +1100

    Get changes only when the negative feedback is being submitted.

--- frontend/src/components/Answer/Answer.tsx
+++ frontend/src/components/Answer/Answer.tsx
@@ -41,6 +41,7 @@ export const Answer = ({
     const [isFeedbackDialogOpen, setIsFeedbackDialogOpen] = useState(false);
     const [showReportInappropriateFeedback, setShowReportInappropriateFeedback] = useState(false);
     const [negativeFeedbackList, setNegativeFeedbackList] = useState<Feedback[]>([]);
+    const [additionalComments, setAdditionalComments] = useState("");
     const appStateContext = useContext(AppStateContext)
     const FEEDBACK_ENABLED = appStateContext?.state.frontendSettings?.feedback_enabled && appStateContext?.state.isCosmosDBAvailable?.cosmosDB;
 
@@ -159,8 +160,11 @@ export const Answer = ({
                 <Checkbox label="References are inaccurate" id={Feedback.WrongCitation} defaultChecked={negativeFeedbackList.includes(Feedback.WrongCitation)} onChange={updateFeedbackList}></Checkbox>
                 <Checkbox label="Response expected but not generated" id={Feedback.ResponseExpectedButNotGenerated} defaultChecked={negativeFeedbackList.includes(Feedback.ResponseExpectedButNotGenerated)} onChange={updateFeedbackList}></Checkbox>
             </Stack>
-            <textarea placeholder="Enter additional comments..." value={additionalComments}  />
-
+            <textarea
+                id="additionalComments"
+                placeholder="Enter additional comments..."
+                value={additionalComments}
+            />
             <div onClick={() => setShowReportInappropriateFeedback(true)} style={{ color: "#115EA3", cursor: "pointer"}}>Report inappropriate content</div>
         </>);
     }
